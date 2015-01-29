f6a8cc1876ab4d1a662a6d8e58ce0dd9b3decf5c
alexed23 <gsnv.alex@gmail.com>
2015-01-29 14:42:27 +0300

    upd

--- manzoor/js/main.js
+++ manzoor/js/main.js
@@ -4,14 +4,49 @@ $(document).ready(function(){
 		fade:true,
 		dots:true,
 		prevArrow:$(".prev"),
-		nextArrow:$(".next")
+		nextArrow:$(".next"),
+		onAfterChange: function(){
+			switch($('.carousel').slickCurrentSlide()) {
+				case 0:
+				$('.caption-stripe').css('left', $('.carousel').width()*0.38);
+				break;
+
+
+
+				case 1:
+				$('.caption-stripe').css('left', $('.carousel').width()*0.43);
+				break;
+
+
+				case 2:
+				$('.caption-stripe').css('left', $('.carousel').width()*0.48);
+				break;
+
+
+				case 3:
+				$('.caption-stripe').css('left', $('.carousel').width()*0.53);
+				break;
+				case 4:
+
+
+				$('.caption-stripe').css('left', $('.carousel').width()*0.58);
+				break;
+
+				default:
+				console.log('test');
+
+			}
+		}
 	});
+
 	if ($( window ).width() > 2000) {
 		viewport = 2000;
 	} else {
 		viewport = $( window ).width();
 	}
 	$('.carousel').width(viewport-$('.form').width());
+
+	$('.caption-stripe').css('left', $('.carousel').width()*0.38);
 });
 
 $(window).resize(function(){
@@ -24,4 +59,37 @@ $(window).resize(function(){
 		viewport = $( window ).width();
 	}
 	$('.carousel').width(viewport-$('.form').width());
-})
+
+
+	switch($('.carousel').slickCurrentSlide()) {
+		case 0:
+		$('.caption-stripe').css('left', $('.carousel').width()*0.38);
+		break;
+
+
+
+		case 1:
+		$('.caption-stripe').css('left', $('.carousel').width()*0.43);
+		break;
+
+
+		case 2:
+		$('.caption-stripe').css('left', $('.carousel').width()*0.48);
+		break;
+
+
+		case 3:
+		$('.caption-stripe').css('left', $('.carousel').width()*0.53);
+		break;
+		case 4:
+
+
+		$('.caption-stripe').css('left', $('.carousel').width()*0.58);
+		break;
+
+		default:
+		console.log('test');
+
+	}
+
+});
