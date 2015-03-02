c28931d26c033f2ff1ef426bfacc78bb4fff8440
alexed23 <gsnv.alex@gmail.com>
2015-03-03 02:56:43 +0300

    young

---
+++ young/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('.collection__items').slick({
+		dots: true,
+		arrows: false
+	});
+
+	$(document).scroll(function(){
+		if ($(document).scrollTop() > 1) {
+			$('header').addClass('header-fixed');
+		}
+
+		else {
+			$('header').removeClass('header-fixed');
+		}
+	});
+
+	new WOW().init();
+});
