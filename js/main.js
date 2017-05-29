4bda4564ca7264cfc14b6f8568f1126ed640858b
Ahmed Guseynov <ahmed.guseynov.23@gmail.com>
2017-05-30 02:24:26 +0300

    Responsive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 $(function() {
+    FastClick.attach(document.body);
+
     $('.portfolio').owlCarousel({
         dots: true,
         arrows: false,
         autoWidth: true,
-        margin: 50
+        margin: 50,
+        center: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            1199: {
+                items: 3
+            }
+        }
     });
-    new WOW().init();
 
+    new WOW().init();
 });
