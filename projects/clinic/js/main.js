941a1f00cb79c4499304c16009ff3a30bed5fcd9
Ahmed Guseynov <alex.guseynov.23@gmail.com>
2019-07-24 10:51:22 +0300

    Update clinic project

--- projects/clinic/js/main.js
+++ projects/clinic/js/main.js
@@ -21,9 +21,30 @@ $(document).ready(function() {
     });
 
     $('.reviews-carousel').owlCarousel({
-        items: 1,
-        nav: true,
-        loop: true,
-        navText: ['<img src="img/icons/arrow-left.svg" />', '<img src="img/icons/arrow-right.svg" />']
+        responsive: {
+            0: {
+                items: 1,
+                loop: true
+            },
+            1200: {
+                items: 1,
+                nav: true,
+                loop: true,
+                navText: ['<img src="img/icons/arrow-left.svg" />', '<img src="img/icons/arrow-right.svg" />']
+            }
+        }
+    });
+
+    $('.header-mobile__burger').click(function() {
+        $('.mobile-menu').addClass('active');
+    });
+
+    $('.header-mobile__close').click(function() {
+        $('.mobile-menu').removeClass('active');
+    });
+
+    $('.read-more').click(function() {
+        $('.mission-toggle').css('max-height', 'none');
+        $('.read-more').remove();
     });
 });
