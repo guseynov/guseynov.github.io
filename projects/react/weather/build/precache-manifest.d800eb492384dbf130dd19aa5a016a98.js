0b7f2b2b02cfd89ac4f374c04e69f1f1d2f11190
guseynov <ahmed.guseynov.23@gmail.com>
2020-01-08 17:07:06 +0300

    Update links

---
+++ projects/react/weather/build/precache-manifest.d800eb492384dbf130dd19aa5a016a98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3009c0af2be681b1e19c5036e3544848",
+    "url": "/projects/react/weather/index.html"
+  },
+  {
+    "revision": "7e28760b0f5ce02b2704",
+    "url": "/projects/react/weather/static/css/main.56f3dd79.chunk.css"
+  },
+  {
+    "revision": "dd2f9e00cbfb3f25dbc4",
+    "url": "/projects/react/weather/static/js/2.25bbefd0.chunk.js"
+  },
+  {
+    "revision": "098d96cd5327dd3fc6707a77e047f86b",
+    "url": "/projects/react/weather/static/js/2.25bbefd0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e28760b0f5ce02b2704",
+    "url": "/projects/react/weather/static/js/main.e2f8b2d3.chunk.js"
+  },
+  {
+    "revision": "7fc12ebb999adee09504",
+    "url": "/projects/react/weather/static/js/runtime-main.ccaf9d0e.js"
+  }
+]);
