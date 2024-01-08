9289c4967d82cc56b83932b82a2e42652fb6f5eb
Alex Guseynov <a.guseinov@sbereducation.ru>
2024-01-08 06:20:45 +0200

    Update title in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
     <header className="grid grid-cols-12 gap-4 text-white py-5 sticky top-0 bg-noise z-10 pt-8 pb-4">
       <div className="col-span-1">Alex Guseynov</div>
       <div className="col-span-1 col-start-3">Based in Israel</div>
-      <div className="col-span-1 col-start-5">React Developer</div>
+      <div className="col-span-1 col-start-5">Frontend Developer</div>
       <div className="col-span-7 flex justify-end">
         <button className="text-gray flex items-center" onClick={scrollToTop}>
           <span className="mr-2">Back to top</span>
